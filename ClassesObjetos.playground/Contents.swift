dcd77df25af5b8b552a06033c9736b936a5b0183
Raí Siqueira <rai93siqueira@gmail.com>
2017-10-31 00:05:09 -0300

    simple Class and Objects

--- ClassesObjetos.playground/Contents.swift
+++ ClassesObjetos.playground/Contents.swift
@@ -25,3 +25,18 @@ var getClass2 = myClass(color: "Red")
 getClass2.getColor()
 
 
+// Heranças
+
+class newClass {
+    var newColor: String = "New color"
+    func color() -> String{
+        return "New color 2"
+    }
+}
+
+class heranca: newClass {
+    func getColor() -> String {
+        return "get Color"
+    }
+}
+
